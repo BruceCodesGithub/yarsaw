9ec951ad2990c72da558b6370020f828cf5ded9a
Bruce <86602706+BruceCodesGithub@users.noreply.github.com>
2021-11-08 20:12:50 +0530

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,10 @@
+from .client import *
+from .utils import *
+
+__title__ = "yarsaw"
+__summary__ = "An async wrapper for Random Stuff API"
+__author__ = "Bruce"
+__version__ = "1.0"
+
+def get_version():
+	return __version__
